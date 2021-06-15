26a0414a3f17f60cd35f86d4661809470838e2e3
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-06-15 19:25:43 +0900

    abort

--- Assets/Scripts/Core/UdpUtility.cs
+++ Assets/Scripts/Core/UdpUtility.cs
@@ -72,6 +72,12 @@ namespace ProjectBlue.NetworkCommunication
                 loopFlg = false;
                 disposable.Dispose();
             }
+
+            if(thread != null)
+            {
+                thread.Abort();
+                thread = null;
+            }
         }
     }
 }
